185ed0eb70dd31b03ba5c74e202eee0407b5cf25
CoderMungan <codermungan@gmail.com>
2025-02-10 10:07:27 +0300

    feat(dependencies): add the dependencies and implement the settings

--- order_service/order_service/settings.py
+++ order_service/order_service/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k%b0e9^vk@*)+m13i5@((nvsz)o+z=u^5%^e%gz%n3@lo%lxe6"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
